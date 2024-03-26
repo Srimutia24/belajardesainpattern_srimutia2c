11f1b58d9abf7a348410ac1a4c3e0f3fbf4bdd37
Srimutia24 <srimutia2121@gmail.com>
2024-03-26 08:56:09 +0700

    first commit

---
+++ src/Duck.java
@@ -0,0 +1,12 @@
+abstract class Duck{
+    void quack (){
+        System.out.println("Qwek qwek");
+    }
+    void swin (){
+        System.out.println("Berenang");
+    }
+    abstract void display();
+    void fly(){
+        System.out.println("Terbang");
+    }
+}
